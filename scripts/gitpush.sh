619c67b82914980b6c627b2c32e392f01dee584a
tangzhiyonggitee <qinglong@@users.noreply.github.com>
2022-02-12 09:13:04 +0800

    qinglong

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -1,8 +1,10 @@
 
 cd ..
 pwd
-git config user.name "tangzhiyonggitee"
-git config user.password "a845538460"
+#git config --global credential.helper store
+git config --global user.name "tangzhiyonggitee"
+git config --global user.password "a845538460"
+git config --global credential.helper store 
 git add .
 git commit -m "qinglong"
 git push
